0b228f40c5206e5e5c04a33deb6f5bbda40e6a9e
olivier van den broek <olivier.vdbroek@gmail.com>
2018-11-21 10:02:52 +0100

    beyond crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    collection do
+      get 'best', to: 'restaurants#best'
+    end
+
+    member do
+      get 'chef', to: 'restaurants#chef'
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    description:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    stars:        5
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    description:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    stars:        4
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
